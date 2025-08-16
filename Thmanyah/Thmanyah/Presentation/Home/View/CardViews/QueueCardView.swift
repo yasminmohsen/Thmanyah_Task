3cc36f6eb02fc14c6daf965e8382968a326fde37
yasminmohsen <50366931+yasminmohsen@users.noreply.github.com>
2025-08-16 17:38:06 +0300

    QueueCardView_UI_Creation

--- Thmanyah/Thmanyah/Presentation/Home/View/CardViews/QueueCardView.swift
+++ Thmanyah/Thmanyah/Presentation/Home/View/CardViews/QueueCardView.swift
@@ -6,24 +6,52 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct QueueCardView: View {
-    var contentItem: UIContentItem = UIContentItem(id: "", title: "hello Q", subtitle: "", imageURL: "", duration: 10, releaseDate: "", episodesCount: 0)
+    var contentItem: UIContentItem = UIContentItem(id: "", title: "hello Q", subtitle: "", imageURL: "https://media.npr.org/assets/img/2018/08/03/npr_tbl_podcasttile_sq-284e5618e2b2df0f3158b076d5bc44751e78e1b6.jpg?s=1400&c=66&f=jpg", duration: 299, releaseDate: "", episodesCount: 0)
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                 .fill(.gray)
-                 .frame(width:120, height: 120)
-                 .frame(maxWidth: .infinity)
-                 .padding(.all, 16)
-                 .overlay {
-                     Text("\(contentItem.title)")
-                 }
-        }
-
+        ZStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 10){
+                KFImage(URL(string: contentItem.imageURL ?? ""))
+                    .resizable()
+                    .frame(width:110, height: 110)
+                    .cornerRadius(12)
+                
+                Text("\(contentItem.title)")
+                    .font(.system(size: 16, weight: .semibold))
+                    .frame(width: 110, alignment: .leading)
+                    .lineLimit(1)
+                    .truncationMode(.tail)
+                HStack(spacing: 10) {
+                    Button {} label: {
+                        HStack {
+                            Image(systemName: "play.fill")
+                                .resizable()
+                                .frame(width: 10, height: 10)
+                                .padding([.leading, .vertical], 8)
+                                Text("\(contentItem.duration?.formattedDuration ?? "")")
+                                    .font(.system(size: 12, weight: .semibold))
+                                    .padding([.trailing, .vertical], 8)
+                                    .multilineTextAlignment(.leading)
+                        }.background {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color(R.color.cardBackground))
+                        }
+                    }.buttonStyle(.plain)
+                    if let date = contentItem.releaseDate {
+                        Text("\(contentItem.releaseDate ?? "")")
+                            .font(.system(size: 12, weight: .semibold))
+                            .multilineTextAlignment(.leading)
+                            .foregroundStyle(Color.gray)
+                    }
+                   
+                }
+            }
+            
+        }.padding(.horizontal, 16)
     }
 }
-
 #Preview {
     QueueCardView()
 }
