868e845d572f5222a7d0e018bd2bdde5fed6af54
yasminmohsen <50366931+yasminmohsen@users.noreply.github.com>
2025-08-17 10:53:49 +0300

    CardsContainerView_UI_Creation

---
+++ Thmanyah/Thmanyah/Presentation/Home/View/CardsContainerView.swift
@@ -0,0 +1,108 @@
+//
+//  CardsContainerView.swift
+//  Thmanyah
+//
+//  Created by Yasmin Mohsen on 17/08/2025.
+//
+
+import SwiftUI
+
+struct CardsContainerView: View {
+    var sections: [SectionItem] = []
+    
+    var loadMoreSections: () async -> Void
+    
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            LazyVStack(alignment: .leading, spacing: 20) {
+                ForEach(sections, id: \.id) { section in
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text(section.name)
+                            .font(.headline)
+                            .frame(alignment: .leading)
+                            .padding(.top, 20)
+                            .padding(.bottom, 12)
+                        
+                        switch section.content {
+                        case .podcasts(let podcasts):
+                            
+                            setUpSections(sectionType: section.type, items: podcasts)
+                            
+                        case .episodes(let episodes):
+                            
+                            setUpSections(sectionType: section.type, items: episodes)
+                            
+                        case .audioBooks(let books):
+                            
+                            setUpSections(sectionType: section.type, items: books)
+                            
+                        case .audioArticles(let articles):
+                            setUpSections(sectionType: section.type, items: articles)
+                        case .none:
+                            Text("No content")
+                                .foregroundColor(.secondary)
+                        }
+                    }.task {
+                        if section.id == sections.last?.id {
+                             await loadMoreSections() 
+                        }
+                    }
+                }
+            }
+        }.padding(.all, 16)
+    }
+    
+    @ViewBuilder
+    func cardView(item: UIContentItem,for layout: SectionLayoutType) -> some View {
+        switch layout {
+        case .square:
+            SquareCardView(contentItem: item)
+        case .bigSquare:
+            BigSquareCardView(contentItem: item)
+        case .queue:
+            QueueCardView(contentItem: item)
+        case .twoLinesGrid:
+            TwoLinesGridCardView(contentItem: item)
+        case .unknown:
+            EmptyView()
+        }
+    }
+    
+    @ViewBuilder
+    func setUpSections(sectionType: SectionLayoutType, items: [any Identifiable]) ->   some View {
+        
+        let uiContentItems = items.map{UIContentItemMapper.map($0)}
+        Group {
+            if sectionType == .bigSquare {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHGrid(rows: [GridItem(.flexible())], spacing: 12) {
+                        ForEach(uiContentItems.indices, id: \.self) { index in
+                            cardView(item: uiContentItems[index], for: .bigSquare)
+                        }
+                    }
+                }
+            } else if sectionType == .twoLinesGrid {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHGrid(rows: [GridItem(.flexible()), GridItem(.flexible())], spacing: 12) {
+                        ForEach(uiContentItems.indices, id: \.self) { index in
+                            cardView(item:uiContentItems[index] ,for: sectionType)
+                        }
+                    }
+                }
+            }
+            else {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 12) {
+                        ForEach(uiContentItems) {item in
+                            cardView(item:item ,for: sectionType)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CardsContainerView(sections: [], loadMoreSections: {})
+}
--- Thmanyah/Thmanyah/Presentation/Home/View/HomeView.swift
+++ Thmanyah/Thmanyah/Presentation/Home/View/HomeView.swift
@@ -36,99 +36,15 @@ struct HomeView: View {
               .background(Color(R.color.lightOrange).opacity(0.3))
             
             if !viewModel.isLoading {
-                ScrollView(showsIndicators: false) {
-                    LazyVStack(alignment: .leading, spacing: 20) {
-                        ForEach(viewModel.sections, id: \.id) { section in
-                            VStack(alignment: .leading, spacing: 0) {
-                                Text(section.name)
-                                    .font(.headline)
-                                    .frame(alignment: .leading)
-                                    .padding(.top, 20)
-                                    .padding(.bottom, 12)
-                                
-                                switch section.content {
-                                case .podcasts(let podcasts):
-                                    
-                                    setUpSections(sectionType: section.type, items: podcasts)
-                                    
-                                case .episodes(let episodes):
-                                    
-                                    setUpSections(sectionType: section.type, items: episodes)
-                                    
-                                case .audioBooks(let books):
-                                    
-                                    setUpSections(sectionType: section.type, items: books)
-                                    
-                                case .audioArticles(let articles):
-                                    setUpSections(sectionType: section.type, items: articles)
-                                case .none:
-                                    Text("No content")
-                                        .foregroundColor(.secondary)
-                                }
-                            }.task {
-                                if section.id == viewModel.sections.last?.id {
-                                    await viewModel.loadMoreSections()
-                                }
-                            }
-                        }
-                    }
-                }.padding(.all, 16)
+                CardsContainerView(sections: viewModel.sections) {
+                    await viewModel.loadMoreSections()
+                }
 
             } else {
                LoadingStateHomeView()
             }
         }.task {
             await viewModel.loadInitialSections()
-        }
-    }
-    
-    @ViewBuilder
-    func cardView(item: UIContentItem,for layout: SectionLayoutType) -> some View {
-        switch layout {
-        case .square:
-            SquareCardView(contentItem: item)
-        case .bigSquare:
-            BigSquareCardView(contentItem: item)
-        case .queue:
-            QueueCardView(contentItem: item)
-        case .twoLinesGrid:
-            TwoLinesGridCardView(contentItem: item)
-        case .unknown:
-            EmptyView()
-        }
-    }
-    
-    @ViewBuilder
-    func setUpSections(sectionType: SectionLayoutType, items: [any Identifiable]) ->   some View {
-        
-        let uiContentItems = items.map{UIContentItemMapper.map($0)}
-        Group {
-            if sectionType == .bigSquare {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    LazyHGrid(rows: [GridItem(.flexible())], spacing: 12) {
-                        ForEach(uiContentItems.indices, id: \.self) { index in
-                            cardView(item: uiContentItems[index], for: .bigSquare)
-                        }
-                    }
-                }
-            } else if sectionType == .twoLinesGrid {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    LazyHGrid(rows: [GridItem(.flexible()), GridItem(.flexible())], spacing: 12) {
-                        ForEach(uiContentItems.indices, id: \.self) { index in
-                            cardView(item:uiContentItems[index] ,for: sectionType)
-                        }
-                    }
-                }
-            }
-            else {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 12) {
-                        ForEach(uiContentItems) {item in
-                            cardView(item:item ,for: sectionType)
-                        }
-                    }
-                }
-            }
         }
     }
 }
